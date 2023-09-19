0e4474502831240943a8873543149a89fc380f06
JOHN P <patchalajohn@gmail.com>
2023-09-19 07:30:36 +0530

    update node version

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
+// const NodeMediaServer = require('node-media-server');
+
+// const config = {
+//   rtmp: {
+//     port: 1935,
+//     chunk_size: 60000,
+//     gop_cache: true,
+//     ping: 30,
+//     ping_timeout: 60
+//   },
+//   http: {
+//     port: 8000,
+//     allow_origin: '*'
+//   }
+// };
+
+// var nms = new NodeMediaServer(config)
+// nms.run();
 const NodeMediaServer = require('node-media-server');
 
 const config = {
   rtmp: {
-    port: 1935,
+    port: 1935, // RTMP port
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
-    ping_timeout: 60
+    ping_timeout: 60,
   },
   http: {
-    port: 8000,
-    allow_origin: '*'
-  }
+    port: 8000, // HTTP port for streaming
+    allow_origin: '*',
+  },
+  https: {
+    port: 8443, // HTTPS port (optional, if you want to use SSL)
+    key: './privatekey.pem',
+    cert: './certificate.pem',
+  },
 };
 
-var nms = new NodeMediaServer(config)
+const nms = new NodeMediaServer(config);
+
+nms.on('prePublish', (id, StreamPath, args) => {
+  // Perform authentication and authorization checks here if needed
+  // Return true to allow publishing or false to deny it
+  return true;
+});
+
 nms.run();
+
+console.log('NodeMediaServer is running.');
